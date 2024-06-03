d10e1e5b8e110068db88086c651ff1cb518c1be4
Marko Vlaic <vlaic.marko@gmail.com>
2024-06-03 21:34:00 +0200

    virtual sensors are now working but still ugly

--- vdev/vdev.py
+++ vdev/vdev.py
@@ -5,62 +5,13 @@ import sys
 from random import random
 import time
 
-sensor_types = ['dht', 'light']
-
-parser = argparse.ArgumentParser(description='Simulates a smart temperature and humidity or light sensor')
-parser.add_argument('-t', '--type', required=True, choices=sensor_types)
-parser.add_argument('-a','--host', help='address of broker host', default='127.0.0.1')
-parser.add_argument('-p', '--port', help="port of broker host", default="1883", type=int)
-parser.add_argument('-o', '--topic', help='mqtt topic on which the readings are published', default='homeassistant/sensor')
-args = parser.parse_args()
-
-type = args.type
-host = args.host
-port = args.port
-topic = args.topic
-
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-print(f'connecting to mqtt broker at {host}:{port}')
-try:
-  client.connect(host, port)
-except:
-  print('connection failed')
-  sys.exit(1)
-
-sensor_topic = f'{topic}/{type}_virt'
-config_topic = f'{sensor_topic}/config'
-
-dht_conf_temp = {
-  "name": "virtual dht temperature",
-  "state_topic": sensor_topic,
-  "unique_id": "virt_dht_temp",
-  "device_class": "temperature",
-  "value_template": "{{ value_json.temperature }}",
-  # "device": {
-  #   "name": "temperature and humidity sensor",
-  #   "identifiers": [
-  #     "dht"
-  #   ]
-  # },
-  "unit_of_measurement": "°C",
-  "force_update": True
-}
-
-dht_conf_hum = {
-  "name": "virtual dht humidity",
-  "state_topic": sensor_topic,
-  "unique_id": "virt_dht_hum",
-  "device_class": "humidity",
-  "value_template": "{{ value_json.humidity }}",
-  # "device": {
-  #   "name": "temperature and humidity sensor",
-  #   "identifiers": [
-  #     "dht"
-  #   ]
-  # },
-  "unit_of_measurement": "%",
-  "force_update": True
-}
+def load_discovery_confs(type):
+  with open(f'{type}_discovery_conf.json', 'r') as f:
+    data = json.load(f)
+    if isinstance(data, list):
+      return data
+    
+    return [data]
 
 def dht_reading_generator():
   temp = random() * 100
@@ -71,28 +22,56 @@ def dht_reading_generator():
   }
   return json.dumps(data)
 
-client.loop_start()
+def light_reading_generator():
+  return random() * 100
 
-if type != 'dht':
-  print('only dht supported')
-  sys.exit(2)
+def start_mqtt_connection(host, port):
+  client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+  client.loop_start()
+  print(f'connecting to mqtt broker at {host}:{port}')
+  try:
+    client.connect(host, port)
+  except:
+    print('connection failed')
+    sys.exit(1)
 
-payload_generator = None
+  return client
 
-if type == 'dht':
-  print('pub')
-  status = client.publish(config_topic, json.dumps(dht_conf_temp), qos=1, retain=True)
-  status.wait_for_publish()
-  #status = client.publish(config_topic, json.dumps(dht_conf_hum), qos=1, retain=True)
-  status.wait_for_publish()
+def publish_discovery_data(client, type):
+  confs = load_discovery_confs(type)
+  # change this
+  if type == 'dht':
+    status = client.publish('homeassistant/sensor/dht_virt_temp/config', json.dumps(confs[0]), qos=1)
+    status.wait_for_publish()
+    status = client.publish('homeassistant/sensor/dht_virt_hum/config ', json.dumps(confs[1]), qos=1)
+    status.wait_for_publish()
+  elif type == 'light':
+    status = client.publish('homeassistant/sensor/light_virt/config', json.dumps(confs[0]), qos=1)
 
-  payload_generator = dht_reading_generator 
+  return confs[0]['state_topic']
+
+sensor_types = ['dht', 'light']
+
+parser = argparse.ArgumentParser(description='Simulates a smart temperature and humidity or light sensor')
+parser.add_argument('-t', '--type', required=True, choices=sensor_types)
+parser.add_argument('-a','--host', help='address of broker host', default='127.0.0.1')
+parser.add_argument('-p', '--port', help="port of broker host", default="1883", type=int)
+args = parser.parse_args()
+
+type = args.type
+host = args.host
+port = args.port
+
+client = start_mqtt_connection(host, port)
+state_topic = publish_discovery_data(client, type)
+
+payload_generator = dht_reading_generator if type == 'dht' else light_reading_generator
 
 while True:
   try:
     payload = payload_generator()
     print(f'publish {payload}')
-    client.publish(sensor_topic, payload)
+    client.publish(state_topic, payload)
     time.sleep(5)
   except KeyboardInterrupt:
     print('stopping virtual device')
