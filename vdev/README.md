4e6c266fe2910ca2bbca1c7d22073ed93dd21066
Marko Vlaic <vlaic.marko@gmail.com>
2024-06-05 11:31:12 +0200

    new and improved clie

---
+++ vdev/README.md
@@ -0,0 +1,74 @@
+# Virtual esp32 device
+# Installation
+```
+$ python -m pip install requirements.txt
+```
+
+## Usage
+```console
+$ python vdev.py -h
+usage: vdev.py [-h] -t {dht,light} [-a HOST] [-p PORT] {add,run,remove} ...
+
+Simulates a smart temperature and humidity or light sensor
+
+positional arguments:
+  {add,run,remove}      Command to run. To see individual command help type <cmd> -h (e.g. vdev.py add -h)
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -t {dht,light}, --type {dht,light}
+  -a HOST, --host HOST  address of broker host
+  -p PORT, --port PORT  port of broker host
+
+```
+
+## Subcommands
+Devices are referenced by their `id`.
+
+```console
+$ python vdev.py add -h
+usage: vdev.py add [-h] -i ID
+
+optional arguments:
+  -h, --help      show this help message and exit
+  -i ID, --id ID  id of the new device
+(venv) [marko@fedora vdev]$ 
+```
+
+```console
+$ python vdev.py run -h
+usage: vdev.py run [-h] -i ID [-m {loop,single}] [-n INTERVAL]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i ID, --id ID        id of the device to remove
+  -m {loop,single}, --mode {loop,single}
+                        Single mode sends one reading and stops the device. Loop mode loops until interrupted.
+  -n INTERVAL, --interval INTERVAL
+                        how many seconds to wait between sending readings in loop mode.
+```
+
+```console
+$ python vdev.py remove -h
+usage: vdev.py remove [-h] -i ID
+
+optional arguments:
+  -h, --help      show this help message and exit
+  -i ID, --id ID  id of the new device
+```
+
+## Examples
+Add a new `dht` device with `id` 0:
+```console
+$ python vdev.py --type dht add --id 0
+```
+
+Run a `light` device with id 4 in a loop with interval 3:
+```console
+$ python vdev.py --type light run --id 4 --interval 3
+```
+
+Remove a `dht` device with id 42:
+```console
+$ python vdev.py --type dht remove --id 42
+```
--- vdev/vdev.py
+++ vdev/vdev.py
@@ -4,14 +4,76 @@ import json
 import sys
 from random import random
 import time
+import copy
 
-def load_discovery_confs(type):
-  with open(f'{type}_discovery_conf.json', 'r') as f:
-    data = json.load(f)
-    if isinstance(data, list):
-      return data
-    
-    return [data]
+def mqtt_connect(args):
+  client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+  client.loop_start()
+  print(f'connecting to mqtt broker at {args.host}:{args.port}')
+  try:
+    client.connect(args.host, args.port)
+  except:
+    print('connection failed')
+    sys.exit(1)
+
+  return client
+
+def default_handler(args):
+  print('invoke with one of the commands: add')
+
+def add_handler(args):
+  client = mqtt_connect(args)
+  confs = []
+
+  if args.type == 'dht':
+    temp_conf = {
+      'name': 'temperature',
+      'unique_id': f'virt_dht_temp{args.id}',
+      'device_class': 'temperature',
+      'state_topic': f'homeassistant/sensor/dht_virt{args.id}/state',
+      'value_template': '{{ value_json.temperature }}',
+      'unit_of_measurement': '°C',
+      'suggested_display_precision': 1,
+      'device': {
+        'identifiers': [
+          f'virt_dht{args.id}'
+        ],
+        'name': f'virtual dht sensor {args.id}'
+      }
+    }
+
+    hum_conf = copy.deepcopy(temp_conf)
+    hum_conf['name'] = 'humidity'
+    hum_conf['unique_id'] = f'virt_dht_hum{args.id}'
+    hum_conf['device_class'] = 'humidity'
+    hum_conf['value_template'] = '{{value_json.humidity}}'
+    hum_conf['unit_of_measurement'] = '%'
+
+    confs.append(temp_conf)
+    confs.append(hum_conf)
+  else:
+    light_conf = {
+      'name': 'light',
+      'unique_id': f'virt_light{args.id}',
+      'device_class': 'illuminance',
+      'state_topic': f'homeassistant/sensor/light_virt{args.id}/state',
+      'unit_of_measurement': '%',
+      'suggested_display_precision': 1,
+      'device': {
+        'identifiers': [
+          f'virt_light{args.id}'
+        ],
+        'name': f'virtual light sensor {args.id}'
+      }
+    }
+    confs.append(light_conf)
+
+  for (i, conf) in enumerate(confs):
+    topic = f'homeassistant/sensor/virt_{args.type}{args.id}{i}/config'
+    status = client.publish(topic, json.dumps(conf), qos=1, retain=True)
+    status.wait_for_publish()
+
+  client.loop_stop()
 
 def dht_reading_generator():
   temp = random() * 100
@@ -25,56 +87,72 @@ def dht_reading_generator():
 def light_reading_generator():
   return random() * 100
 
-def start_mqtt_connection(host, port):
-  client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-  client.loop_start()
-  print(f'connecting to mqtt broker at {host}:{port}')
-  try:
-    client.connect(host, port)
-  except:
-    print('connection failed')
-    sys.exit(1)
+def run_handler(args):
+  client = mqtt_connect(args)
+  reading_generator = dht_reading_generator if args.type == 'dht' else light_reading_generator
+  topic = f'homeassistant/sensor/{args.type}_virt{args.id}/state'
 
-  return client
+  while True:
+    try:
+      reading = reading_generator()
+      print(f'send reading {reading}')
+      status = client.publish(topic, reading, qos=1)
+      status.wait_for_publish()
 
-def publish_discovery_data(client, type):
-  confs = load_discovery_confs(type)
-  # change this
-  if type == 'dht':
-    status = client.publish('homeassistant/sensor/dht_virt_temp/config', json.dumps(confs[0]), qos=1)
+      if args.mode == 'single':
+        break
+
+      time.sleep(args.interval)
+    except KeyboardInterrupt:
+      break
+  
+  client.loop_stop()
+
+def remove_handler(args):
+  client = mqtt_connect(args)
+  topics = []
+  if args.type == 'dht':
+    topics.append(f'homeassistant/sensor/virt_{args.type}{args.id}0/config')
+    topics.append(f'homeassistant/sensor/virt_{args.type}{args.id}1/config')  
+  else:
+    topics.append(f'homeassistant/sensor/virt_{args.type}{args.id}0/config')
+  
+  for topic in topics:
+    status = client.publish(topic, '', qos=1)
     status.wait_for_publish()
-    status = client.publish('homeassistant/sensor/dht_virt_hum/config ', json.dumps(confs[1]), qos=1)
-    status.wait_for_publish()
-  elif type == 'light':
-    status = client.publish('homeassistant/sensor/light_virt/config', json.dumps(confs[0]), qos=1)
 
-  return confs[0]['state_topic']
+  client.loop_stop()
 
-sensor_types = ['dht', 'light']
+def main():
+  sensor_types = ['dht', 'light']
 
-parser = argparse.ArgumentParser(description='Simulates a smart temperature and humidity or light sensor')
-parser.add_argument('-t', '--type', required=True, choices=sensor_types)
-parser.add_argument('-a','--host', help='address of broker host', default='127.0.0.1')
-parser.add_argument('-p', '--port', help="port of broker host", default="1883", type=int)
-args = parser.parse_args()
+  parser = argparse.ArgumentParser(description='Simulates a smart temperature and humidity or light sensor')
+  parser.add_argument('-t', '--type', required=True, choices=sensor_types)
+  parser.add_argument('-a','--host', help='address of broker host', default='127.0.0.1')
+  parser.add_argument('-p', '--port', help="port of broker host", default="1883", type=int)
+  parser.set_defaults(handler=default_handler)
 
-type = args.type
-host = args.host
-port = args.port
+  subparsers = parser.add_subparsers(help='Command to run. To see individual command help type <cmd> -h (e.g. vdev.py add -h)')
 
-client = start_mqtt_connection(host, port)
-state_topic = publish_discovery_data(client, type)
+  parser_add = subparsers.add_parser('add')
+  parser_add.add_argument('-i', '--id', help='id of the new device', required=True, type=int)
+  parser_add.set_defaults(handler=add_handler)
+  
+  run_modes = ['loop', 'single']
+  parser_run = subparsers.add_parser('run')
+  parser_run.add_argument('-i', '--id', help='id of the device to remove', required=True, type=int)
+  parser_run.add_argument('-m', '--mode', help='Single mode sends one reading and stops the device. Loop mode loops until interrupted.', choices=run_modes, default='loop')
+  parser_run.add_argument('-n', '--interval', help='how many seconds to wait between sending readings in loop mode.', default=5, type=int)
+  parser_run.set_defaults(handler=run_handler)
 
-payload_generator = dht_reading_generator if type == 'dht' else light_reading_generator
+  parser_remove = subparsers.add_parser('remove')
+  parser_remove.add_argument('-i', '--id', help='id of the new device', required=True, type=int)
+  parser_remove.set_defaults(handler=remove_handler)
 
-while True:
-  try:
-    payload = payload_generator()
-    print(f'publish {payload}')
-    client.publish(state_topic, payload)
-    time.sleep(5)
-  except KeyboardInterrupt:
-    print('stopping virtual device')
-    break
 
-client.loop_stop()
+  args = parser.parse_args()
+  args.handler(args)
+
+if __name__ == '__main__':
+  main()
+
