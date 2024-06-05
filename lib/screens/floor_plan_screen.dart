2f04bd39f850a83203f12e851c5130a703df2261
Marko Bakic <marko.bakic@fer.hr>
2024-06-05 19:35:02 +0200

    Added dynamic widget creation

--- lib/screens/floor_plan_screen.dart
+++ lib/screens/floor_plan_screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:luminar_control_app/providers/scenes_provider.dart';
+import 'package:luminar_control_app/screens/room_control_screen.dart';
 
-class FloorPlanScreen extends StatelessWidget {
+class FloorPlanScreen extends ConsumerWidget {
   const FloorPlanScreen({super.key});
 
+  static const List<double> heightTopDividers = [13, 6.5];
+  static const List<double> widthLeftDividers = [6, 1.85];
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final scenes = ref.watch(scenesProvider);
     Size screenSize = MediaQuery.of(context).size;
     double imageHorizontalPadding = 50;
     return Scaffold(
@@ -19,72 +26,91 @@ class FloorPlanScreen extends StatelessWidget {
             'Track and manage your home!',
           ),
           const SizedBox(height: 50, width: double.infinity),
-          Stack(
-            children: [
-              Container(
-                color: Colors.red.withOpacity(.5),
-                child: Image.asset(
-                  'assets/floor_plan/floor_plan_p.png',
-                  width: screenSize.width - imageHorizontalPadding,
+          Center(
+            child: Stack(
+              children: [
+                Center(
+                  child: Image.asset(
+                    'assets/floor_plan/floor_plan_p.png',
+                    width: screenSize.width - imageHorizontalPadding,
+                  ),
                 ),
-              ),
-              Positioned(
-                top: 50,
-                left: 45,
-                child: Opacity(
-                  opacity: .8,
-                  child: SizedBox(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(minimumSize: const Size(30, 40)),
-                      onPressed: () => {},
-                      child: const Icon(Icons.light),
+                ...scenes.asMap().entries.map(
+                  (entry) {
+                    final index = entry.key;
+                    final scene = entry.value;
+                    return Positioned(
+                      top: screenSize.height / heightTopDividers[index],
+                      left: screenSize.width / widthLeftDividers[index],
+                      child: Opacity(
+                        opacity: .8,
+                        child: SizedBox(
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(minimumSize: const Size(30, 40)),
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return RoomControlScreen(
+                                    entityIDs: scene.attributes.entityIds,
+                                    frendlyName: scene.attributes.friendlyName,
+                                  );
+                                },
+                              );
+                            },
+                            child: const Icon(Icons.light),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                /*
+                Positioned(
+                  top: screenSize.height / 13,
+                  left: screenSize.width / 6,
+                  child: Opacity(
+                    opacity: .8,
+                    child: SizedBox(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(minimumSize: const Size(30, 40)),
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return RoomControlScreen(entityIDs: scenes.first.attributes.entityIds);
+                            },
+                          );
+                        },
+                        child: const Icon(Icons.light),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Positioned(
-                top: 103,
-                right: 90,
-                child: Opacity(
-                  opacity: .8,
-                  child: SizedBox(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(minimumSize: const Size(30, 40)),
-                      onPressed: () => {},
-                      child: const Icon(Icons.light),
+                Positioned(
+                  top: screenSize.height / 6.5,
+                  left: screenSize.width / 1.85,
+                  child: Opacity(
+                    opacity: .8,
+                    child: SizedBox(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(minimumSize: const Size(30, 40)),
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return RoomControlScreen(entityIDs: scenes[1].attributes.entityIds);
+                            },
+                          );
+                        },
+                        child: const Icon(Icons.light),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Positioned(
-                top: 170,
-                left: 45,
-                child: Opacity(
-                  opacity: .8,
-                  child: SizedBox(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(minimumSize: const Size(30, 40)),
-                      onPressed: () => {},
-                      child: const Icon(Icons.light),
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 80,
-                right: 15,
-                child: Opacity(
-                  opacity: .8,
-                  child: SizedBox(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(minimumSize: const Size(30, 40)),
-                      onPressed: () => {},
-                      child: const Icon(Icons.light),
-                    ),
-                  ),
-                ),
-              ),
-            ],
+                */
+              ],
+            ),
           ),
         ],
       ),
